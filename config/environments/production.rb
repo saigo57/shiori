82ec0cc1c38186e7f0c798218e504eca5e0097fa
Saigo1997 <a.saigo1997@gmail.com>
2021-01-13 22:48:07 +0900

    hosturl修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :ses
-  config.action_mailer.default_url_options = { host: 'shiori-portfolio.work' }
+  config.action_mailer.default_url_options = { host: 'www.shiori-portfolio.work' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
