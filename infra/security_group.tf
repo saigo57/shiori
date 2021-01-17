ed58002fda8e8709404a1e665cf5acf777a4a5d7
Saigo1997 <a.saigo1997@gmail.com>
2021-01-18 01:57:20 +0900

    terraform構築完了

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -28,3 +28,58 @@ resource "aws_security_group" "web_sg" {
     Name = "shiori-${terraform.workspace}-web-sg"
   }
 }
+
+resource "aws_security_group" "db_sg" {
+  name        = "shiori-${terraform.workspace}-db-sg"
+  description = "for db"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "shiori-${terraform.workspace}-db-sg"
+  }
+}
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "shiori-${terraform.workspace}-bastion-sg"
+  description = "for bastion"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "shiori-${terraform.workspace}-bastion-sg"
+  }
+}
