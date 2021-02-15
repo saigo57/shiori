3ba5a0f9da73271427e3cfc5ceeece24b246c807
Saigo1997 <a.saigo1997@gmail.com>
2021-02-15 22:56:59 +0900

    Revert "path_styleをtrueにしてasset_hostを追加"
    This reverts commit c0cdc4ce7def2e022a77607bf1a5a06b7527c409.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,14 +4,13 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.asset_host = 'https://shiori-prod-backet.s3-ap-northeast-1.amazonaws.com/'
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
       region: ENV['S3_REGION'],
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      path_style: true
+      path_style: false
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
