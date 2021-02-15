12d3a1d5c1a7c9079c04258e77e89bd07e3e21cd
Saigo1997 <a.saigo1997@gmail.com>
2021-02-16 00:11:57 +0900

    fog_publicを追加

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,6 +4,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_public = true
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
