c1bc878b2eb3e6c13e37388a8229960251abd56d
Saigo1997 <a.saigo1997@gmail.com>
2021-02-17 00:45:05 +0900

    S3_HOSTを追加

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_public = true
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
       region: ENV['S3_REGION'],
+      host: ENV['S3_HOST'],
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
       path_style: true
@@ -16,3 +20,5 @@ if Rails.env.production?
     config.fog_directory = ENV['S3_BUCKET']
   end
 end
+ 
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
