7558b18383c409eec1fb689f7dadb24eb9eb2c16
Saigo1997 <a.saigo1997@gmail.com>
2021-06-02 01:34:50 +0900

    pumaの設定を変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
-min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
-threads min_threads_count, max_threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
-rails_env = ENV.fetch('RAILS_ENV', 'development')
-environment rails_env
-port        ENV.fetch('PORT', 3000)
+preload_app!
 
-unless rails_env == 'production'
-  pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
-  plugin :tmp_restart
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 end
