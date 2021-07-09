feef54dbf389a23141db908a571ba414d9041311
Saigo1997 <a.saigo1997@gmail.com>
2021-07-10 00:36:53 +0900

    メール送信をgmailで実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,18 +67,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'shiori-movie.herokuapp.com' }
   config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.SendGrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: ENV['GMAIL_USER_NAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :login
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
