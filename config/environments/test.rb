9230715e5892e18a5d98c5c0cd3f4b9a9e05450d
Saigo1997 <a.saigo1997@gmail.com>
2021-10-17 21:03:51 +0900

    fixes #100 ドメインをroute53で取得したものにする 修正

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.forgery_protection_origin_check = false
+
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 end
