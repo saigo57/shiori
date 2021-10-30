f594238c92c2dab75cf72bc68ecbaf6ed395275a
Saigo1997 <a.saigo1997@gmail.com>
2021-10-31 03:17:49 +0900

    fixes #103 ゲスト機能を削除する

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  devise_scope :user do
-    post '/users/guest_sign_in', to: 'users/sessions#new_guest'
-  end
+  # devise_scope :user do
+  #   post '/users/guest_sign_in', to: 'users/sessions#new_guest'
+  # end
   root to: 'home#index'
   resources :media_manages do
     patch 'restore', on: :member
