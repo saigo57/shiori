0db093fa50abb92358fc14d29d5f9b6caff86e6f
Saigo1997 <a.saigo1997@gmail.com>
2022-01-08 16:20:35 +0900

    staging環境作成

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'shiori.amanoiwato.link' }
+  config.action_mailer.default_url_options = { host: ENV['SHIORI_HOST_NAME'] }
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
     domain: 'gmail.com',
