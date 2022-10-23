4ba70577b1fca85fac7faab88facd8f787253da4
Saigo1997 <a.saigo1997@gmail.com>
2022-10-23 17:31:58 +0900

    fixes #150 インフラをAWSに変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@shiori-portfolio.work'
+  config.mailer_sender = 'shiori.noreply@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
--- config/puma.rb
+++ config/puma.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
+threads min_threads_count, max_threads_count
 
-preload_app!
+rails_env = ENV.fetch('RAILS_ENV', 'development')
+environment rails_env
+port        ENV.fetch('PORT', 3000)
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+if rails_env == 'production'
+  app_root = File.expand_path('..', __dir__)
+  bind "unix://#{app_root}/tmp/sockets/puma.sock"
+else
+  pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
+  plugin :tmp_restart
 end
